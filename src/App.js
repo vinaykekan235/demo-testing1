d6a7e6eff1633e5a08a52308671c6fef0675716f
vinaykekan235 <vinaykekan235@gmail.com>
2024-05-21 14:29:06 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hi how are you i ma vinay
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
